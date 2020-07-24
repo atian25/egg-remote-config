cf0e10daa6450cd8a302f560bd4811c20cf02dae
TZ | 天猪 <atian25@qq.com>
2020-07-24 11:48:05 +0800

    test: fix ci at win (#2)

--- test/remote-config.test.js
+++ test/remote-config.test.js
@@ -4,7 +4,7 @@ const mock = require('egg-mock');
 const assert = require('assert');
 const asserFile = require('assert-file');
 const path = require('path');
-const { rimraf } = require('mz-modules');
+const { rimraf, sleep } = require('mz-modules');
 
 describe('test/remote-config.test.js', () => {
   let app;
@@ -19,9 +19,11 @@ describe('test/remote-config.test.js', () => {
   after(() => app.close());
   afterEach(mock.restore);
 
-  it('should works', () => {
+  it('should works', async () => {
     assert(app.config.foo === 'egg-remote-config');
     assert(app.config.sub.a === 'b');
+    // for win
+    await sleep('1s');
     asserFile.fail(path.join(app.config.rundir, 'remote_config.json'));
   });
 });
