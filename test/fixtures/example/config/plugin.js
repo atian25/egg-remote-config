4dfa3ef83812c63c9175b88d75413db04ee79d03
TZ | 天猪 <atian25@qq.com>
2020-07-27 18:14:51 +0800

    test: make sure old style lifecycle is work (#3)

--- test/fixtures/example/config/plugin.js
+++ test/fixtures/example/config/plugin.js
@@ -1,3 +1,9 @@
 'use strict';
 
+const path = require('path');
+
 exports.static = false;
+
+exports.test = {
+  path: path.join(__dirname, '../../test-plugin'),
+};
--- test/remote-config.test.js
+++ test/remote-config.test.js
@@ -25,5 +25,15 @@ describe('test/remote-config.test.js', () => {
     // for win
     await sleep('1s');
     asserFile.fail(path.join(app.config.rundir, 'remote_config.json'));
+
+    // make sure test-plugin is load before remote-config
+    const plugins = app.loader.orderPlugins.map(it => it.name);
+    const testIndex = plugins.indexOf('test');
+    const remoteIndex = plugins.indexOf('remoteConfig');
+    assert(testIndex < remoteIndex && testIndex !== -1 && remoteIndex !== -1);
+
+    // then check test-plugin use the newest config from remote-config
+    assert(app.test.foo === 'egg-remote-config');
+    assert(app.test.sub.a === 'b');
   });
 });
